5a90ed7c3037778fd51f26b506e1afee2cf62018
akghosh111 <ghoshkiran2017@gmail.com>
2026-06-09 20:16:52 +0530

    add drizzle & betterauth

--- backend/src/app/index.js
+++ backend/src/app/index.js
@@ -1,4 +1,7 @@
+import cors from "cors";
 import express from "express";
+import { auth } from "../lib/auth.js";
+import { toNodeHandler } from "better-auth/node";
 
 
 export function createApplication() {
@@ -6,6 +9,16 @@ export function createApplication() {
 
     app.use(express.json());
 
+
+    app.use(
+    cors({
+        origin: ["http://localhost:5173", "http://localhost:3000"], // Added common frontend ports
+        methods: ["GET", "POST", "PUT", "DELETE"], 
+        credentials: true,
+    })
+    );
+
+    app.all(/^\/api\/auth(?:\/.*)?$/, (req, res) => betterAuthHandler(req, res));
 
     app.get("/health", (req, res) => {
         return res.json({message: "This server is healthy"});
